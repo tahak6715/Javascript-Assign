f4b68a0d22800b56d65d0b4b942d3316a74c54b4
Taha khan <tahak6715@gmail.com>
2022-09-03 02:54:56 +0500

    Work accomplished

---
+++ app.js
@@ -0,0 +1,410 @@
+   // CHAPTER-1 / EXERCISE-1 //
+
+//alert("Hello World!");
+//alert("Hi there, Sailor!");
+//alert("You won!");
+//alert("hi");
+//alert("Hi Love!");
+
+
+  // CHAPTER-2 / EXERCISE-2 //
+
+//var punchline = "The accountant";
+//alert(punchline);  
+//var teammate;
+//teammate = "pak";
+//var bestMan = "charlie";
+//bestMan = "baby";
+//alert(bestMan);
+//var name;
+
+
+
+
+   // CHAPTER_3 / EXERCISE-3 //
+
+//var merchTotal = 100;
+//var shippingCharge = 10;
+//var orderTotal = merchTotal + shippingCharge;
+//alert(orderTotal);
+//var bignumb = 1300000;
+//var num = 13;
+//var sum;
+//sum =  4 + 5;
+//alert(sum);
+
+
+   // CHAPTER_4 / EXERCISE-4 //
+
+//var averageHumanInteligence;
+//var _$;
+//var human;                          
+
+
+   // CHAPTER_5 / EXERCISE-5 //
+
+//var num = 20 % 6;
+//alert(num);  
+//var num1 = 10000 * 2000;
+//alert(num1);
+//var doubledNum = 12 + 12;
+//var leftOver = num % 3;   
+//alert(leftOver);
+
+
+  // CHAPTER-6 / EXERCISE-6 //
+
+//var newNum = 4;  
+//var num = newNum++;
+
+//alert(num);  
+//var x=50;
+//var y = x++;
+//alert(y);
+//var x = 50;
+//var yy = --x;
+//alert(yy);
+//num1 = 22;
+//var num = --num1;
+//alert(num);
+//var num2 = num1++;
+//alert(num2);
+//var area++;
+//alert(area);
+
+
+  // CHAPTER-7 / EXERCISE-7 //
+
+//num = 2 + (2 * 6);
+//alert(num);
+//calculatedNum = (2 + 2) * 6;
+//alert(calculatedNum);
+//Num = (2 + 2) * (4 + 2);
+//alert(Num);
+//caalculatedNum = ((2 + 2) * 4) + 2;
+//alert(caalculatedNum);
+//var units = 2 + (2 * 4) + 10;
+//alert(units);
+//var pressure = (4 / 2) * 4;
+//alert(pressure);
+  
+   // CHAPTER-8 / EXERCISE-8 //
+   // CONCATINATION //
+
+//var num = "2" + "2";
+//alert(num);
+//alert("33" + 3);
+//var cost = "The cost: " + 59.80;
+//alert(cost);
+//var firstPart = "Hello, ";
+//var secondPart = "Dolly!";
+//var show = firstPart + secondPart; 
+//alert(show);
+//var userName = "Taha";
+//alert("Hi, " + userName);
+
+    // CHAPTER-9 / EXERCISE-9 //
+    // PROMPT //
+
+
+//var firstname = prompt("Enter your name : ")
+//alert("Your name is : " + firstname);
+//var country = prompt("Country?", "China");
+//var name = prompt("Enter your name?");
+//alert(name);
+//var question = "What are you"
+//var response = prompt(question);
+//var response = prompt("question", "AqRfT");
+
+
+   // CHAPTER-10 / EXERCISE-10 //
+   // IF STATEMENT //
+
+
+//var firstName = "Taha";
+//var nickname = "Taha";
+
+//if (firstName === nickname) {
+//   alert("Your name is, Taha");
+//}
+//var pCode = "10010";
+//if (pCode === "10010") {
+//   city = "Karachi";
+//   alert(city);
+// } 
+//var baby = 1;
+//if (baby === 1) {
+ //  baby = 2;
+//   alert(baby);
+//}  
+
+    
+     // CHAPTER-11 / EXERCISE-11 //
+     // COMPARISION OPERATOR //
+
+
+//var num1 = 5;
+//var num2 = 1;     
+//if (num1 >= num2){
+//   alert("num1 is greter");
+//}   
+//if (num1 !== num2) {
+//   alert("Num1 is different from num2");
+//}  
+//if (num1 > num2) {
+//   alert("num1 is bara");
+//}
+
+
+
+     // CHAPTER-12 / EXERCISE-12 //
+
+//var a = 2;
+//var b = 2;
+//if (a !== b) {
+//   alert("NOT OK");
+//}
+//else if (a === b) {
+//   alert("OK");     
+//}
+//else{
+//   alert("I DONT HAVE WORDS");
+//}  
+
+
+     // CHAPTER-13 / EXERCISE-13 //
+
+//var weight = 400;
+//var time = 3;     
+//if (weight > 300 && time < 6) {
+//   alert("Come to our tryout!");
+//}
+//else {
+//   alert("Come to our cookout!");
+//}
+
+//var age = 77;
+//if (age > 65 || (age < 21 && res === "U.S.")) {
+//   alert("HI");
+//}   
+
+
+    // CHAPTER-14 / EXERCISE-14 //
+
+
+//var c = 4;
+//var d = 4;
+//var a = 3;
+//var b = 5;
+//var x = 4;
+//var y = 4;
+
+//if (c === d) {
+//   if (x === y) {
+//      alert("Hry!");
+//   }
+//   else if (a === b) {
+//      g = h;
+//   }
+//   else {
+//      e = f;
+//   }
+//}
+//else {
+//   alert("HII");
+//}    
+
+
+
+     // CHAPTER-15 / EXERCISE-15 //
+     // ARRAY //
+
+
+//var numAsString = ["1" , "2" , "3"];
+//alert(numAsString[0]);
+//var nums = [3 , 4 , 5];
+//alert(nums);
+
+//var cities = ["Karachi" , "Lahore" , "islamabad"];
+//alert(cities[1]);
+
+
+
+     // CHAPTER-16 / EXERCISE-16 //
+
+
+//var names = ["taha" , "khan" , "ali" , "sami"];
+//names.pop();
+//alert(names);
+//names.push("khan1" , "khan2");
+//alert(names);
+
+
+
+
+      // CHAPTER-17 / EXERCISE-17 //
+
+
+//var apps = ["fb" , "insta" , "twitter" , "linkdin"];
+//apps.shift();
+//alert(apps); 
+//apps.unshift("fb" , "skype") ;
+//alert(apps);    
+
+
+//var ppar = ["ware" , "dana" , "lays" , "wavy"];
+//ppar.splice(2,0,"ali","wali");
+//alert(ppar);
+  
+//var abc = ppar.slice(2, 4);
+//alert(abc);
+
+
+    // CHAPTER-18 / EXERCISE-18 //
+
+
+
+    
+//for (var i = 0; i <= 11; i++) {
+//   alert("OHKSYY");
+//}   
+
+
+//for (var i = 0; i < 7; i++) {
+//   alert("k.o");
+//}  
+
+
+      // CHAPTER-19 / EXERCISE-19 //
+
+      //var a = ["sm001","sm002","sm002","sm003","sm004","sm005","sm006","sm007","sm008","sm009","sm0010","sm0011","sm0012","sm0013","sm0014","sm0015","sm0016","sm0018","sm0019","sm0020"]
+      //var b =[prompt("enter your roll!")];
+      //var flag = false;
+      
+      //for (var i=0; i<20; i++){
+        //  console.log(a[i]);
+         // if (a[i] == b){
+           //   flag =true;
+             // break;
+              
+         // }
+          
+      //}
+    //  if(flag == true){
+       //   alert("student found!");
+      //}
+      //else{
+      //    alert("student not found");
+     // }
+ 
+  
+
+        // CHAPTER-20 / EXERCISE-20 //
+
+
+//var c = ["a" , "b" , "c" , "d" ,"e"];
+//var d = [1 , 2 , 3 , 4 , 5 , 6];
+
+
+//for(var i=0; i<c.length; i++){
+//    for(var j=0; j<d.length; i++){
+//        console.log(c[i]+d[j]);
+//    }
+//}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
